8e6f80487d689379c9ddeb946a4b6f905f0f991a
unknown <emile.bernard.2@ulaval.ca>
2019-03-22 14:47:26 -0400

    Added probabilistic Hough lines

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # OpenCV-Line-Detection
-This project contains a basic python script to run opencv-python with Hough Line Transform to detect lines in an image. This script shows the product of a basic Canny edge detection and after shows the product of line detection.
+This project contains a basic python script to run opencv-python with Hough Line Transform to detect lines in an image. This script shows the product of a basic Canny edge detection, the product of Hough line detection and the product of a probabilistic Hough line detection.
 
 ## Previews
 ![Preview1](./documentation/preview/Preview1.JPG?raw=true "Preview1")
 ![Preview2](./documentation/preview/Preview2.JPG?raw=true "Preview2")
 ![Preview3](./documentation/preview/Preview3.JPG?raw=true "Preview3")
+![Preview4](./documentation/preview/Preview4.JPG?raw=true "Preview4")
 
 ## How it work's
-[Hough Line Transform](https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html)
+- [Hough Line Transform](https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_houghlines/py_houghlines.html)
+- [Hough transform](https://en.wikipedia.org/wiki/Hough_transform)
 
 ## How to setup
 - Install opencv-python
--- main.py
+++ main.py
@@ -1,33 +1,81 @@
 import cv2
 import numpy as np
+import glob
 
-image = cv2.imread('assets/images/chess_board.jpg')
+def getGrayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Grayscale and Canny Edges extracted
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray, 100, 170, apertureSize = 3)
+def getEdges(grayscale):
+    return cv2.Canny(grayscale, 100, 170, apertureSize = 3)
 
-cv2.imshow("Edges", edges)
-cv2.waitKey(0)
+def showDefaultImage(filepath):
+    image = cv2.imread(filepath)
+    cv2.imshow("Default Image", image)
+    cv2.waitKey(0)
 
-# Run HoughLines using a rho accuracy of 1 pixel
-# theta accuracy of np.pi / 180 which is 1 degree
-# Our line threshold is set to 240 (number of points on line)
-lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
+def showGrayscaleAndCannyEdges(filepath):
+    image = cv2.imread(filepath)
+    grayscale = getGrayscale(image)
+    edges = getEdges(grayscale)
 
-# Convert lines to the format required by cv.lines (requiring end points)
-for line in lines:
-    rho, theta = line[0]
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a * rho
-    y0 = b * rho
-    x1 = int(x0 + 1000 * (-b))
-    y1 = int(y0 + 1000 * (a))
-    x2 = int(x0 - 1000 * (-b))
-    y2 = int(y0 - 1000 * (a))
-    cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    cv2.imshow("Grayscale And Canny Edges", edges)
+    cv2.waitKey(0)
 
-cv2.imshow('Hough Lines', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def showHoughLines(filepath, rhoAccuracy, theta, lineThreshold):
+    image = cv2.imread(filepath)
+    grayscale = getGrayscale(image)
+    edges = getEdges(grayscale)
+
+    lines = cv2.HoughLines(edges, rhoAccuracy, theta, lineThreshold)
+
+    lineColor = (0, 0, 255)
+    lineThickness = 1
+
+    # Convert lines to the format required by cv.lines (end points)
+    for line in lines:
+        rho, theta = line[0]
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a * rho
+        y0 = b * rho
+        firstLinePoint = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
+        secondLinePoint = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
+        cv2.line(image, firstLinePoint, secondLinePoint, lineColor, lineThickness)
+
+    cv2.imshow('Hough Lines', image)
+    cv2.waitKey(0)
+
+def showProbabilisticHoughLines(filepath, rhoAccuracy, theta, lineThreshold):
+    image = cv2.imread(filepath)
+    grayscale = getGrayscale(image)
+    edges = getEdges(grayscale)
+
+    minLineLength = 5
+    maxGapBetweenLines = 10
+    lines = cv2.HoughLinesP(edges, rhoAccuracy, theta, lineThreshold, minLineLength, maxGapBetweenLines)
+
+    lineColor = (0, 255, 0)
+    lineThickness = 2
+
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv2.line(image, (x1, y1), (x2, y2), lineColor, lineThickness)
+
+    cv2.imshow('Probabilistic Hough Lines', image)
+    cv2.waitKey(0)
+
+for filepath in glob.iglob('assets/images/*.jpg'):
+    showDefaultImage(filepath)
+    showGrayscaleAndCannyEdges(filepath)
+
+    # Rho accuracy of 1 pixel
+    rhoAccuracy = 1
+    # Theta accuracy of np.pi / 180 = 1 degree
+    theta = np.pi/180
+    # Line threshold (number of points on line)
+    lineThreshold = 200
+
+    showHoughLines(filepath, rhoAccuracy, theta, lineThreshold)
+    showProbabilisticHoughLines(filepath, rhoAccuracy, theta, lineThreshold)
+
+    cv2.destroyAllWindows()
